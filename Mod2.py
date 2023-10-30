fff6603db809d575e06c4d11453b01c566061890
nkivytech <99282769+nkivytech@users.noreply.github.com>
2023-10-30 14:21:13 -0400

    Add files via upload

---
+++ Mod2.py
@@ -0,0 +1,47 @@
+"""
+    document markups
+"""
+
+text = "use backslash \
+to add multiple lines \
+as a single line"
+
+print(text)
+
+#if statements
+
+x = 7
+
+if x % 2 == 0:
+
+    print("even")
+
+else:
+
+    print("odd")
+
+
+#nested ifs
+
+number = 100
+
+if number > 50:
+
+    if number %2 == 0:
+
+        print("Big and Even")
+
+    else:
+
+        print("Big and Odd")
+
+else:
+
+    if number %2 == 0:
+
+        print("Small and Even")
+
+    else:
+
+        print("Small and Odd")
+
